0f7abade4054fde65225f986cf49ca1fa34fda01
AvitalZemnucha <113101913+AvitalZemnucha@users.noreply.github.com>
2025-04-28 16:09:51 +0300

    updated test plan and e2e test

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,8 @@
-API_BASE_URL = "http://127.0.0.1:8000"
+import os
+
+# Dynamically pick the API port: use environment variable if exists, otherwise default to 8000
+API_PORT = os.getenv("API_PORT", "8000")
+API_BASE_URL = f"http://127.0.0.1:{API_PORT}"
 
 ########User##############
 API_LOGIN_URL = f"{API_BASE_URL}/login"
@@ -13,7 +17,6 @@ API_PANEL_ADMIN = f"{API_BASE_URL}/panel"
 API_ORDERS_ADMIN = f"{API_BASE_URL}/panel/orders"
 API_ORDERS_STATUS_ADMIN = f"{API_BASE_URL}/panel/orders/status"
 API_UPDATE_STATUS_ADMIN = f"{API_BASE_URL}/panel/orders/update-status"
-
 
 #####Credit Card######
 card_payload = {
